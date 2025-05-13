91f1a96a5cd56ce2fef45f2459e468c7e59a7c07
Evan <evan@Evans-MacBook-Pro.local>
2025-05-13 13:57:05 +0300

    fiex

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+// Dynamically set the base URL based on environment
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8000/api",
-  withCredentials: true,
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? "https://spacekc.onrender.com/api"  // Production base URL
+    : "http://localhost:8000/api",         // Local development base URL
+  withCredentials: true, // If you need to handle cookies/sessions
 });
 
 export default apiRequest;
