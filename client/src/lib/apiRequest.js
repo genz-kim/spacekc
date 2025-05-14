3120b2e873406477a3fc7e0e92ac1893a094237c
Evan <evan@Evans-MacBook-Pro.local>
2025-05-14 11:06:57 +0300

    cors3

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   baseURL: process.env.NODE_ENV === 'production'
-    ? "https://spacekc.onrender.com/api"   // ✅ Actual backend URL on Render
+    ? "https://spacekc-ajy7.onrender.com/api"   // ✅ Actual backend URL on Render
     : "http://localhost:8000/api",         // ✅ Local backend for dev
   withCredentials: true, // ✅ Needed for cookies/auth headers
 });
