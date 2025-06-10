b37ec3c4dc6b04dc131d919db87673098d1ee378
Evan <evan@Evans-MacBook-Pro.local>
2025-06-10 09:11:52 +0300

    fixxx

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiRequest = axios.create({
   baseURL:
     import.meta.env.PROD
-      ? "https://homes-api-7oh9.onrender.com/api" // ✅ your actual backend API
+      ? "https://homes-7oh9.onrender.com/api" // ✅ your actual backend API
       : "http://localhost:8000/api",              // ✅ for local development
   withCredentials: true,
 });
