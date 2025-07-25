c0b9f94f8e1b2d13ba2653a05cd95db478c58ec9
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-25 09:48:34 +0300

    Refactor

--- client/src/routes/singlePage/singlePage.jsx
+++ client/src/routes/singlePage/singlePage.jsx
@@ -100,34 +100,6 @@ function SinglePage() {
 			<div className="w-full flex flex-col md:flex-row gap-4">
 				<div className="w-full md:w-3/4">
 					<Slider images={post?.images || []} />
-					{/* <div className="info">
-						<div className="top">
-							<div className="post">
-								<h1>{post?.title}</h1>
-								<div className="address">
-									<img
-										src="/pin.png"
-										alt=""
-									/>
-									<span>{post?.address}</span>
-								</div>
-								<div className="price">ksh {post?.price}</div>
-							</div>
-							<div className="user">
-								<img
-									src={post?.user?.avatar}
-									alt=""
-								/>
-								<span>{post?.user?.username}</span>
-							</div>
-						</div>
-						<div
-							className="bottom"
-							dangerouslySetInnerHTML={{
-								__html: DOMPurify.sanitize(post?.postDetail?.desc),
-							}}
-						></div>
-					</div> */}
 				</div>
 				<div className="w-full md:w-1/4">
 					<AgentSection post={post} />
@@ -136,52 +108,7 @@ function SinglePage() {
 
 			<div className="">
 				<PropertyDetails post={post} />
-				{/* <div className="listVertical">
-            <div className="feature">
-              <img src="/utility.png" alt="" />
-              <div className="featureText">
-                <span>Utilities</span>
-                {post?.postDetail?.utilities === "owner" ? (
-                  <p>Owner is responsible</p>
-                ) : (
-                  <p>Tenant is responsible</p>
-                )}
-              </div>
-            </div>
-            <div className="feature">
-              <img src="/pet.png" alt="" />
-              <div className="featureText">
-                <span>Pet Policy</span>
-                {post?.postDetail?.pet === "allowed" ? (
-                  <p>Pets Allowed</p>
-                ) : (
-                  <p>Pets not Allowed</p>
-                )}
-              </div>
-            </div>
-            <div className="feature">
-              <img src="/fee.png" alt="" />
-              <div className="featureText">
-                <span>Income Policy</span>
-                <p>{post?.postDetail?.income}</p>
-              </div>
-            </div>
-          </div>
-          <p className="title">Sizes</p>
-          <div className="sizes">
-            <div className="size">
-              <img src="/size.png" alt="" />
-              <span>{post?.postDetail?.size} sqft</span>
-            </div>
-            <div className="size">
-              <img src="/bed.png" alt="" />
-              <span>{post?.bedroom} beds</span>
-            </div>
-            <div className="size">
-              <img src="/bath.png" alt="" />
-              <span>{post?.bathroom} bathroom</span>
-            </div>
-          </div>
+				{/*
           <p className="title">Nearby Places</p>
           <div className="listHorizontal">
             <div className="feature">
