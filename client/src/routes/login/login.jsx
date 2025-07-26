cbff4bb531c57bfcfee3f39ebf0c3d6a70301f84
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-26 16:12:12 +0300

    Refactor login and register component

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,77 +1,133 @@
-import "./login.scss";
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import apiRequest from "../../lib/apiRequest";
-import { AuthContext } from "../../context/AuthContext";
-import { useSignIn } from "@clerk/clerk-react"; // Import Clerk SignIn hook
+import { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import apiRequest from '../../lib/apiRequest';
+import { AuthContext } from '../../context/AuthContext';
+import { useSignIn } from '@clerk/clerk-react'; // Import Clerk SignIn hook
+import { FaFacebook } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 function Login() {
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const { updateUser } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const { signIn, isLoaded } = useSignIn(); // Clerk sign-in
+	const [error, setError] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const { updateUser } = useContext(AuthContext);
+	const navigate = useNavigate();
+	const { signIn, isLoaded } = useSignIn(); // Clerk sign-in
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-    setError("");
-    const formData = new FormData(e.target);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setIsLoading(true);
+		setError('');
+		const formData = new FormData(e.target);
 
-    const username = formData.get("username");
-    const password = formData.get("password");
+		const username = formData.get('username');
+		const password = formData.get('password');
 
-    try {
-      const res = await apiRequest.post("/auth/login", {
-        username,
-        password,
-      });
-      updateUser(res.data);
-      navigate("/list");
-    } catch (err) {
-      console.error("Login Error:", err);
-      setError(err.response?.data?.message || "An unexpected error occurred.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+		try {
+			const res = await apiRequest.post('/auth/login', {
+				username,
+				password,
+			});
+			updateUser(res.data);
+			navigate('/list');
+		} catch (err) {
+			console.error('Login Error:', err);
+			setError(err.response?.data?.message || 'An unexpected error occurred.');
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  // Google Sign-In with Clerk
-  const handleGoogleLogin = async () => {
-    if (!isLoaded) return;
-    try {
-      await signIn.authenticateWithRedirect({
-        strategy: "oauth_google",
-        redirectUrl: "/list", // Redirect after login
-      });
-    } catch (err) {
-      console.error("Google Sign-in Error:", err);
-      setError("Failed to sign in with Google.");
-    }
-  };
+	// Google Sign-In with Clerk
+	const handleGoogleLogin = async () => {
+		if (!isLoaded) return;
+		try {
+			await signIn.authenticateWithRedirect({
+				strategy: 'oauth_google',
+				redirectUrl: '/list', // Redirect after login
+			});
+		} catch (err) {
+			console.error('Google Sign-in Error:', err);
+			setError('Failed to sign in with Google.');
+		}
+	};
 
-  return (
-    <div className="login">
-      <div className="formContainer">
-        <form onSubmit={handleSubmit}>
-          <h1>Welcome back</h1>
-          <input name="username" required minLength={3} maxLength={15} type="text" placeholder="Username" />
-          <input name="password" required minLength={3} maxLength={15} type="password" placeholder="Password" />
-          <button disabled={isLoading}>Login</button>
-          {error && <span>{error}</span>}
-          <button className="google-btn" onClick={handleGoogleLogin}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGHQ31PQmO35YNg3EL8HUhyIJXjBX2YigTFjj9roeDLYN2XexiCj4Hq2MZ3Gyu-dmyN84&usqp=CAU" alt="Google" width="20" height="20" />
-          Sign In with Google
-        </button>
-          <Link to="/register">{"Don't"} have an account?</Link>
-        </form>
-        
-      </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full h-full flex flex-col justify-center items-center py-5 md:py-20">
+			<div className="flex flex-col gap-4 w-full md:w-[486px] bg-white rounded-lg shadow-lg p-4 md:p-8">
+				<h1 className="text-lg font-semibold pb-2">Welcome back</h1>
+				<form
+					onSubmit={handleSubmit}
+					className="flex flex-col gap-4"
+				>
+					<input
+						name="username"
+						required
+						minLength={3}
+						maxLength={15}
+						type="text"
+						placeholder="Username"
+						className="w-full py-2 bg-white border border-[#E5E7EB] rounded-md px-3 focus:outline-none focus:border-accent"
+					/>
+					<input
+						name="password"
+						required
+						minLength={3}
+						maxLength={15}
+						type="password"
+						placeholder="Password"
+						className="w-full py-2 bg-white border border-[#E5E7EB] rounded-md px-3 focus:outline-none focus:border-accent"
+					/>
+					<div className="w-full">
+						<button
+							disabled={isLoading}
+							className="w-full bg-accent rounded-sm p-1.5 text-white font-bold cursor-pointer disabled:cursor-none disabled:bg-gray-400"
+						>
+							Login
+						</button>
+						{error && <span className="text-red-500">{error}</span>}
+						<p className="text-sm pt-2">
+							Don't have an account?{' '}
+							<Link
+								to="/register"
+								className="text-blue-500"
+							>
+								Register
+							</Link>
+						</p>
+					</div>
+
+					<div className="flex flex-col gap-4">
+						<div className="flex items-center gap-4">
+							<hr className="flex-grow border-t border-gray-300" />
+							<span className="text-xs text-gray-500 whitespace-nowrap">
+								Or login with
+							</span>
+							<hr className="flex-grow border-t border-gray-300" />
+						</div>
+						<div className="flex gap-3">
+							<button
+								onClick={handleGoogleLogin}
+								className="flex gap-2 p-1.5 bg-white border border-[#E5E7EB] rounded-md w-full text-center justify-center items-center"
+							>
+								<FcGoogle size={25} />
+								Google
+							</button>
+							<button
+								className="flex gap-2 p-1.5 bg-white border border-[#E5E7EB] rounded-md w-full text-center justify-center items-center"
+								onClick={handleGoogleLogin}
+							>
+								<FaFacebook
+									size={25}
+									className="text-blue-500"
+								/>
+								Facebook
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,76 +1,136 @@
-import "./register.scss";
-import { useState } from "react";
-import apiRequest from "../../lib/apiRequest";
-import { Link, useNavigate } from "react-router-dom";
-import { useSignUp } from "@clerk/clerk-react"; // Import Clerk SignUp hook
+import { useState } from 'react';
+import apiRequest from '../../lib/apiRequest';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignUp } from '@clerk/clerk-react'; // Import Clerk SignUp hook
+import { FaFacebook } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
 
 function Register() {
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
-  const { signUp, isLoaded } = useSignUp(); // Clerk sign-up
+	const [error, setError] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
+	const { signUp, isLoaded } = useSignUp(); // Clerk sign-up
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("");
-    setIsLoading(true);
-    const formData = new FormData(e.target);
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setError('');
+		setIsLoading(true);
+		const formData = new FormData(e.target);
 
-    const username = formData.get("username");
-    const email = formData.get("email");
-    const password = formData.get("password");
+		const username = formData.get('username');
+		const email = formData.get('email');
+		const password = formData.get('password');
 
-    try {
-      await apiRequest.post("/auth/register", {
-        username,
-        email,
-        password,
-      });
-      navigate("/login");
-    } catch (err) {
-      setError(err.response?.data?.message || "An unexpected error occurred.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
+		try {
+			await apiRequest.post('/auth/register', {
+				username,
+				email,
+				password,
+			});
+			navigate('/login');
+		} catch (err) {
+			setError(err.response?.data?.message || 'An unexpected error occurred.');
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  // Google Sign-Up with Clerk
-  const handleGoogleSignUp = async () => {
-    if (!isLoaded) return;
-    try {
-      await signUp.authenticateWithRedirect({
-        strategy: "oauth_google",
-        redirectUrl: "/list", // Redirect after sign-up
-      });
-    } catch (err) {
-      console.error("Google Sign-up Error:", err);
-      setError("Failed to sign up with Google.");
-    }
-  };
+	// Google Sign-Up with Clerk
+	const handleGoogleSignUp = async () => {
+		if (!isLoaded) return;
+		try {
+			await signUp.authenticateWithRedirect({
+				strategy: 'oauth_google',
+				redirectUrl: '/list', // Redirect after sign-up
+			});
+		} catch (err) {
+			console.error('Google Sign-up Error:', err);
+			setError('Failed to sign up with Google.');
+		}
+	};
 
-  return (
-    <div className="register">
-      <div className="formContainer">
-        <form onSubmit={handleSubmit}>
-          <h1>Create an Account</h1>
-          <input name="username" type="text" placeholder="Username" required />
-          <input name="email" type="text" placeholder="Email" required />
-          <input name="password" type="password" placeholder="Password" required />
-          <button disabled={isLoading}>Register</button>
-          {error && <span>{error}</span>}
-          <button className="google-btn" onClick={handleGoogleSignUp}>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGHQ31PQmO35YNg3EL8HUhyIJXjBX2YigTFjj9roeDLYN2XexiCj4Hq2MZ3Gyu-dmyN84&usqp=CAU" alt="Google" width="20" height="20" />
-          Sign up with Google
-        </button>
-          <Link to="/login">Already have an account?</Link>
-        </form>
+	return (
+		<div className="w-full h-full flex flex-col justify-center items-center py-5 md:py-20">
+			<div className="flex flex-col gap-4 w-full md:w-[486px] bg-white rounded-lg shadow-lg p-4 md:p-8">
+				<h1 className="text-lg font-semibold pb-2">Create an Account</h1>
+				<form
+					onSubmit={handleSubmit}
+					className="flex flex-col gap-4"
+				>
+					<input
+						className="w-full py-2 bg-white border border-[#E5E7EB] rounded-md px-3 focus:outline-none focus:border-accent"
+						name="username"
+						type="text"
+						placeholder="Username"
+						required
+					/>
+					<input
+						className="w-full py-2 bg-white border border-[#E5E7EB] rounded-md px-3 focus:outline-none focus:border-accent"
+						name="email"
+						type="text"
+						placeholder="Email"
+						required
+					/>
+					<input
+						className="w-full py-2 bg-white border border-[#E5E7EB] rounded-md px-3 focus:outline-none focus:border-accent"
+						name="password"
+						type="password"
+						placeholder="Password"
+						required
+					/>
 
-      </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
-      </div>
-    </div>
-  );
+					<div className="w-full">
+						<button
+							disabled={isLoading}
+							className="w-full bg-accent rounded-sm p-1.5 text-white font-bold cursor-pointer disabled:cursor-none disabled:bg-gray-400"
+						>
+							Register
+						</button>
+						{error && <span className="text-red-500">{error}</span>}
+						<p className="text-sm pt-2">
+							Already have an account?{' '}
+							<Link
+								to="/login"
+								className="text-blue-500"
+							>
+								Log in
+							</Link>
+						</p>
+					</div>
+
+					<div className="flex flex-col gap-4">
+						<div className="flex items-center gap-4">
+							<hr className="flex-grow border-t border-gray-300" />
+							<span className="text-xs text-gray-500 whitespace-nowrap">
+								Or register with
+							</span>
+							<hr className="flex-grow border-t border-gray-300" />
+						</div>
+
+						<div className="flex gap-3">
+							<button
+								className="flex gap-2 p-1.5 bg-white border border-[#E5E7EB] rounded-md w-full text-center justify-center items-center"
+								onClick={handleGoogleSignUp}
+							>
+								<FcGoogle size={25} />
+								Google
+							</button>
+							<button
+								className="flex gap-2 p-1.5 bg-white border border-[#E5E7EB] rounded-md w-full text-center justify-center items-center"
+								onClick={handleGoogleSignUp}
+							>
+								<FaFacebook
+									size={25}
+									className="text-blue-500"
+								/>
+								Facebook
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Register;
