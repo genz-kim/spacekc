527ac230b9f7fd59a99b47ac899a622fc7ca9c01
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-26 16:12:47 +0300

    Create similar properties component

---
+++ client/src/components/property-page/SimilarProperties.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SimilarProperties = () => {
+  return (
+		<div className="flex flex-col gap-4 py-4 md:py-10 w-full justify-center items-center">
+			<h1 className="text-3xl font-bold">Similar Properties</h1>
+			{/* // TODO: Logic for displaying */}
+			<p>Display similar properties</p>
+		</div>
+	);
+}
+
+export default SimilarProperties
