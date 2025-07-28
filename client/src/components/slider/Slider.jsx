0ec0ebf1079bfcf35dd055203a35878bf8b65948
Catherine Vuthi <vuthi.esther@gmail.com>
2025-07-28 20:21:03 +0300

    Edit slider.jsx

--- client/src/components/slider/Slider.jsx
+++ client/src/components/slider/Slider.jsx
@@ -21,38 +21,62 @@ function Slider({ images }) {
   };
 
   return (
-    <div className="slider">
-      {imageIndex !== null && (
-        <div className="fullSlider">
-          <div className="arrow" onClick={() => changeSlide("left")}>
-            <img src="/arrow.png" alt="" />
-          </div>
-          <div className="imgContainer">
-            <img src={images[imageIndex]} alt="" />
-          </div>
-          <div className="arrow" onClick={() => changeSlide("right")}>
-            <img src="/arrow.png" className="right" alt="" />
-          </div>
-          <div className="close" onClick={() => setImageIndex(null)}>
-            X
-          </div>
-        </div>
-      )}
-      <div className="bigImage">
-        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
-      </div>
-      <div className="smallImages">
-        {images.slice(1).map((image, index) => (
-          <img
-            src={image}
-            alt=""
-            key={index}
-            onClick={() => setImageIndex(index + 1)}
-          />
-        ))}
-      </div>
-    </div>
-  );
+		<div className="slider">
+			{imageIndex !== null && (
+				<div className="fullSlider">
+					<div
+						className="arrow"
+						onClick={() => changeSlide('left')}
+					>
+						<img
+							src="/arrow.png"
+							alt=""
+						/>
+					</div>
+					<div className="imgContainer">
+						<img
+							src={images[imageIndex]}
+							alt=""
+						/>
+					</div>
+					<div
+						className="arrow"
+						onClick={() => changeSlide('right')}
+					>
+						<img
+							src="/arrow.png"
+							className="right"
+							alt=""
+						/>
+					</div>
+					<div
+						className="close"
+						onClick={() => setImageIndex(null)}
+					>
+						X
+					</div>
+				</div>
+			)}
+			<div className="bigImage">
+				<img
+					src={images[0]}
+					alt=""
+					onClick={() => setImageIndex(0)}
+					className="w-full max-h-72"
+				/>
+			</div>
+			<div className="smallImages">
+				{images.slice(1).map((image, index) => (
+					<img
+						src={image}
+						alt=""
+						key={index}
+						onClick={() => setImageIndex(index + 1)}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default Slider;
